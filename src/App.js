87ad62376145b827ab8e09385a6098ef696c0bff
clarkfennell <clarkfennell@mac.com>
2022-09-24 13:46:58 +0100

    simple styling structure added with variables

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+import classes from './../styles/App.module.scss';
+
 export const App = () => {
-  return <h1>Hello World!</h1>
+  return (
+    <div className={classes.container}>
+      <h1>Hello World!</h1>
+    </div>
+  )
 }
