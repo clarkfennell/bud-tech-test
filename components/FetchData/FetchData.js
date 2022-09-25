b57d9f94804251821488bbe5bfc1f23f583e9234
clarkfennell <clarkfennell@mac.com>
2022-09-25 01:36:33 +0100

    chaning api url from http to https for vercel depoy

--- components/FetchData/FetchData.js
+++ components/FetchData/FetchData.js
@@ -3,7 +3,7 @@
  * @returns An object with a property called data.
  */
 export const FetchData = async () => {
-    const response = await fetch(`http://www.mocky.io/v2/5c62e7c33000004a00019b05`);
+    const response = await fetch(`https://www.mocky.io/v2/5c62e7c33000004a00019b05`);
     const data = await response.json();
     return data;
 };
